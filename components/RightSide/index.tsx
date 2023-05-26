3ee4874da96f6473537a2b1ef31b9178d9cd1c1b
JONGMIN KIM <bellmin9321@gmail.com>
2023-05-27 01:13:17 +0900

    ✅ Add: RightSide 컴포넌트 추가 및 스타일 추가

---
+++ components/RightSide/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './styles.module.scss';
+
+function RightSide({ children }: { children: React.ReactNode }) {
+  return <div className={styles.rightside}>{children}</div>;
+}
+
+export default RightSide;
