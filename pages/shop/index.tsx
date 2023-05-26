74316b81a6d5ab528aba3ee3416b150889f1de41
JONGMIN KIM <bellmin9321@gmail.com>
2023-05-27 01:20:27 +0900

    ✅ Add: Shop 동적 페이지 구현

---
+++ pages/shop/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Content from '@/components/Content';
+import ItemsSection from '@/components/ItemsSection';
+
+function Shop() {
+  return (
+    <Content>
+      <ItemsSection />
+    </Content>
+  );
+}
+
+export default Shop;
