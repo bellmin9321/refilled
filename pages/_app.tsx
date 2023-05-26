e5c7a04faa5512ba8c072d24548cba549edd6f28
JONGMIN KIM <bellmin9321@gmail.com>
2023-05-27 01:07:19 +0900

    🌳 Refactor: Layout 및 global.scss 스타일 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,18 @@ import { AppProps } from 'next/app';
 import { Suspense } from 'react';
 import { QueryClientProvider } from 'react-query';
 
-import '@/styles/globals.css';
+import '@/styles/globals.scss';
 
 import { queryClient } from '@/lib/api/queryClient';
+import Layout from '@/components/Layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <Suspense fallback={<div>Loading...</div>}>
-        <Component {...pageProps} />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
       </Suspense>
     </QueryClientProvider>
   );
