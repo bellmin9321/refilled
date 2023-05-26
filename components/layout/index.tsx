1d8d4dc37761a721b6f336116af13f31757288c6
JONGMIN KIM <bellmin9321@gmail.com>
2023-05-27 01:12:05 +0900

    ✨ Feat: Layout UI 추가

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,12 +1,15 @@
 import * as React from 'react';
 
 import IndexPage from './IndexPage';
+import LeftSide from '../LeftSide';
+import RightSide from '../RightSide';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <div id="layoutRoot">
       <IndexPage />
-      {children}
-    </>
+      <LeftSide />
+      <RightSide>{children}</RightSide>
+    </div>
   );
 }
