aeed03b20b1ad5355912fed4545b0adc5316d337
JONGMIN KIM <bellmin9321@gmail.com>
2023-05-26 17:05:29 +0900

    ✅ Add: Layout 및 기본 HomePage 추가

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Layout from '@/components/Layout';
+
+function HomePage() {
+  return (
+    <Layout>
+      <div>hello</div>
+    </Layout>
+  );
+}
+
+export default HomePage;
