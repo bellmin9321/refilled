ef980c5b84498c18bc0224327f59c3aaa66773da
JONGMIN KIM <bellmin9321@gmail.com>
2023-05-27 23:49:10 +0900

    🌳 Refactor: Cart 상태에 따른 UI 구현 및 CartItem 이용한 리팩토링

--- components/CartItemsSection/index.tsx
+++ components/CartItemsSection/index.tsx
@@ -1,13 +1,31 @@
 import React from 'react';
 import styles from './styles.module.scss';
+import { cartItemsHandler } from '@/context/CartItemsContext';
+import { ItemType } from '@/types';
+import CartItem from '../CartItem';
+import Link from 'next/link';
 
 function CartItemsSection() {
+  const { cart } = cartItemsHandler();
+
+  if (!cart.length) {
+    return (
+      <div className={styles.blankCartSection}>
+        <h1>Refilled</h1>
+        <div className={styles.subTitle}>
+          {`장바구니에 담긴 상품이 없습니다.\n상품을 추가해 보세요.`}
+        </div>
+        <Link href="/shop" className={styles.moveShop}>
+          상품 둘러보기
+        </Link>
+      </div>
+    );
+  }
+
   return (
     <div className={styles.cartItemsSection}>
-      <h1 className={styles.title}>Refilled</h1>
-      <div className={styles.subTitle}>
-        {`장바구니에 담긴 제품이 없습니다.\n 제품을 추가해 보세요.`}
-      </div>
+      {cart.length > 0 &&
+        cart.map((item: ItemType) => <CartItem key={item.id} item={item} />)}
     </div>
   );
 }
