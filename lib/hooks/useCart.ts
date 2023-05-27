02e31f04a3e79dc8dd41f0e3678759df2345410c
JONGMIN KIM <bellmin9321@gmail.com>
2023-05-28 02:58:44 +0900

    ✨ Feat: id와 option이 같은 Item 중복 조건 추가

--- lib/hooks/useCart.ts
+++ lib/hooks/useCart.ts
@@ -4,25 +4,25 @@ import { useState } from 'react';
 
 function useCart() {
   const [cart, setCart] = useState<ItemType[]>([]);
-  const { option, setOption, setModalType } = modalHandler();
+  const { option, setModalType } = modalHandler();
 
   const addItemToCart = (item: ItemType) => {
     const newCart = [...cart];
-    const duplicated = newCart.find((v) => v.id === item.id);
+    const duplicated = newCart.find(
+      (v) => v.id === item.id && v.option === option,
+    );
 
-    if (option === 'default' && item.productOptions.length) {
+    if (!option && item.productOptions.length) {
       alert('옵션을 선택해주세요');
       return;
     }
-
-    setOption('default');
 
     if (duplicated) {
       alert('선택한 상품이 장바구니에 있습니다.');
       return;
     }
 
-    setCart([...newCart, item]);
+    setCart([...newCart, { ...item, option }]);
     setModalType('CART');
   };
 
