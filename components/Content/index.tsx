9516c15879e329c19a85e9a056dbe4889f53b2e7
JONGMIN KIM <bellmin9321@gmail.com>
2023-05-27 23:42:32 +0900

    ✅ Add: 모달 컴포넌트 추가

--- components/Content/index.tsx
+++ components/Content/index.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import styles from './styles.module.scss';
 import Header from '../Header';
+import { ModalHandler } from '@/context/ModalContext';
+import ModalLayout from '../ModalLayout';
+import OptionSelector from '../OptionSelector';
 
 function Content({ children }: { children: React.ReactNode }) {
+  const { modal } = ModalHandler();
+
   return (
     <div className={styles.content}>
       <Header />
       {children}
+      {modal && (
+        <ModalLayout>
+          <OptionSelector />
+        </ModalLayout>
+      )}
     </div>
   );
 }
