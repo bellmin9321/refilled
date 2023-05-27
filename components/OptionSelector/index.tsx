f3fc55442d45440ddf5d70f9ccbaf079a1eb8429
JONGMIN KIM <bellmin9321@gmail.com>
2023-05-27 23:52:46 +0900

    ✨ Feat: Modal 상태에 따른 UI 적용 및 장바구니에 Item 넣기 구현

---
+++ components/OptionSelector/index.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import styles from './styles.module.scss';
+import { ModalHandler } from '@/context/ModalContext';
+import { cartItemsHandler } from '@/context/CartItemsContext';
+import Link from 'next/link';
+
+function OptionSelector() {
+  const { selectedItem, modalType, handleModal, setModalType } = ModalHandler();
+  const { addItemToCart } = cartItemsHandler();
+
+  const handleRoute = () => {
+    setModalType('OPTION');
+    handleModal();
+  };
+
+  return (
+    <>
+      {modalType === 'CART' ? (
+        <div className={styles.confirmCartBox}>
+          <div className={styles.imageBox}>
+            <img src={selectedItem.imageUrl} width={150} height={150} />
+            <div>{selectedItem.name}</div>
+            <span>장바구니에 상품이 담겼습니다.</span>
+          </div>
+          <Link href="/cart" className={styles.footer} onClick={handleRoute}>
+            바로가기
+          </Link>
+        </div>
+      ) : (
+        <div className={styles.optionSelectBox}>
+          <div className={styles.title}>{selectedItem.name}</div>
+          <select
+            className={styles.select}
+            name="tagOption"
+            disabled={!selectedItem.productOptions.length}
+          >
+            <option value="">
+              {`옵션 ${selectedItem.productOptions.length ? '선택' : '없음'}`}
+            </option>
+            {selectedItem.productOptions.map((option) => {
+              return (
+                <option key={option.id} value={option.name}>
+                  {option.name}
+                </option>
+              );
+            })}
+          </select>
+          <div
+            className={styles.footer}
+            onClick={() => addItemToCart(selectedItem)}
+          >
+            장바구니 담기
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default OptionSelector;
