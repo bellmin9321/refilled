2b016f71fc751f24490fa99961896792efa9ec69
JONGMIN KIM <bellmin9321@gmail.com>
2023-05-27 23:50:58 +0900

    ✨ Feat: Cart 상태에 따른 갯수 및 금액 변동 적용

--- components/CartFooter/index.tsx
+++ components/CartFooter/index.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import styles from './styles.module.scss';
+import { cartItemsHandler } from '@/context/CartItemsContext';
 
 function CartFooter() {
+  const { cart } = cartItemsHandler();
+  const totalPrice = cart.reduce((sum, item) => sum + item.price, 0);
+
   return (
     <div className={styles.footer}>
-      <div className={styles.innerBox}>{`5개 구매하기`}</div>
+      <div className={styles.innerBox}>
+        <span className={styles.totalPrice}>{`${
+          cart.length
+        }개 | ${totalPrice.toLocaleString('ko-KR')}원`}</span>
+        <span className={styles.buy}>&nbsp;구매하기</span>
+      </div>
     </div>
   );
 }
