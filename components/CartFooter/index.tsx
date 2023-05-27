b5efd63e2f91a535a284fd782adcf38af32b6013
JONGMIN KIM <bellmin9321@gmail.com>
2023-05-28 02:19:17 +0900

    ✅ Add: 구매하기 버튼 클릭 시 라우팅 및 alert 추가

--- components/CartFooter/index.tsx
+++ components/CartFooter/index.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import styles from './styles.module.scss';
 import { cartItemsHandler } from '@/context/CartItemsContext';
+import Link from 'next/link';
 
 function CartFooter() {
-  const { cart } = cartItemsHandler();
+  const { cart, setCart } = cartItemsHandler();
   const totalPrice = cart.reduce((sum, item) => sum + item.price, 0);
+
+  const handleClick = () => {
+    alert('구매를 완료하였습니다.');
+    setCart([]);
+  };
 
   return (
     <div className={styles.footer}>
-      <div className={styles.innerBox}>
+      <Link href="/shop" className={styles.innerBox} onClick={handleClick}>
         <span className={styles.totalPrice}>{`${
           cart.length
         }개 | ${totalPrice.toLocaleString('ko-KR')}원`}</span>
         <span className={styles.buy}>&nbsp;구매하기</span>
-      </div>
+      </Link>
     </div>
   );
 }
