5ab6ee5976617792fbd0366326dfa0344327835e
JONGMIN KIM <bellmin9321@gmail.com>
2023-05-27 23:50:14 +0900

    ✨ Feat: CartItem 컴포넌트 구현

---
+++ components/CartItem/index.tsx
@@ -0,0 +1,56 @@
+import { ItemProps } from '@/types';
+import React from 'react';
+import styles from './styles.module.scss';
+import { IoMdClose } from 'react-icons/io';
+import { cartItemsHandler } from '@/context/CartItemsContext';
+
+function CartItem({ item }: ItemProps) {
+  const { deleteItemInCart } = cartItemsHandler();
+
+  return (
+    <div className={styles.itemLayout}>
+      <div key={item.id} className={styles.itemBox}>
+        <img
+          src={item.imageUrl}
+          className={styles.image}
+          width={60}
+          height={60}
+        />
+        <div>
+          {item.tag.text && (
+            <span
+              className={styles.tag}
+              style={
+                item.tag.text === 'BEST'
+                  ? { color: 'white', backgroundColor: '#256CEE' }
+                  : { color: '#333333', backgroundColor: '#E6E6E6' }
+              }
+            >
+              {item.tag.text}
+            </span>
+          )}
+          <div className={styles.name}>{item.name}</div>
+          <div className={styles.desc}>{item.desc}</div>
+          <div className={styles.priceBox}>
+            <div
+              className={styles.originPrice}
+            >{`${item.originPrice.toLocaleString('ko-KR')}원`}</div>
+            <div>
+              <span className={styles.percent}>
+                {`${100 - Math.floor((item.price / item.originPrice) * 100)}%`}
+              </span>
+              <span className={styles.price}>{`${item.price.toLocaleString(
+                'ko-KR',
+              )}원`}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <span onClick={() => deleteItemInCart(item)}>
+        <IoMdClose size={20} />
+      </span>
+    </div>
+  );
+}
+
+export default CartItem;
