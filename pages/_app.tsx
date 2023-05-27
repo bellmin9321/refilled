85ae0dee831ee44eee57af52a0d3b33f2db32fdc
JONGMIN KIM <bellmin9321@gmail.com>
2023-05-27 23:36:25 +0900

    ✅ Add: ModalContext, CartItemsContext provider 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,15 +6,21 @@ import '@/styles/globals.scss';
 
 import { queryClient } from '@/lib/api/queryClient';
 import Layout from '@/components/Layout';
+import { ModalContextProvider } from '@/context/ModalContext';
+import { CartItemsContextProvider } from '@/context/CartItemsContext';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </Suspense>
+      <ModalContextProvider>
+        <CartItemsContextProvider>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </Suspense>
+        </CartItemsContextProvider>
+      </ModalContextProvider>
     </QueryClientProvider>
   );
 }
