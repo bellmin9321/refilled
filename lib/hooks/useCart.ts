23521fdc84f1479f6d9163723f631cd24bf472e0
JONGMIN KIM <bellmin9321@gmail.com>
2023-05-28 02:18:08 +0900

    ✅ Add: 상태 및 조건 추가

--- lib/hooks/useCart.ts
+++ lib/hooks/useCart.ts
@@ -1,22 +1,29 @@
-import { ModalHandler } from '@/context/ModalContext';
+import { modalHandler } from '@/context/ModalContext';
 import { ItemType } from '@/types';
 import { useState } from 'react';
 
 function useCart() {
   const [cart, setCart] = useState<ItemType[]>([]);
-  const { setModalType } = ModalHandler();
+  const { option, setOption, setModalType } = modalHandler();
 
   const addItemToCart = (item: ItemType) => {
     const newCart = [...cart];
     const duplicated = newCart.find((v) => v.id === item.id);
+
+    if (option === 'default' && item.productOptions.length) {
+      alert('옵션을 선택해주세요');
+      return;
+    }
+
+    setOption('default');
 
     if (duplicated) {
       alert('선택한 상품이 장바구니에 있습니다.');
       return;
     }
 
-    setModalType('CART');
     setCart([...newCart, item]);
+    setModalType('CART');
   };
 
   const deleteItemInCart = (item: ItemType) => {
