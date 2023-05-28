abc6c2a6093ecafb1aebd73a3f09e78c2fc4175d
JONGMIN KIM <bellmin9321@gmail.com>
2023-05-29 02:04:26 +0900

    ✨ Feat: redux store 구현

---
+++ store/index.ts
@@ -0,0 +1,26 @@
+import { configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+
+import { persistReducer, persistStore } from 'redux-persist';
+import { reducers } from './reducers';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart', 'modal'],
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+export const persistor = persistStore(store);
+export type RootState = ReturnType<typeof store.getState>;
+
+export default store;
